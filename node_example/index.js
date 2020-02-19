890b44f221c753993062e416f26c656b2fca416f
heniam1 <heniam2012@gmail.com>
2020-02-18 23:54:21 -0500

    Simple Node Example

--- node_example/index.js
+++ node_example/index.js
@@ -1,1 +1,23 @@
+var rect = {
+      perimeter: (x,y) => (2*(x+y)),
+      area:(x,y) => (x*y)
+};
 
+
+
+function solveRect(l,b){
+  console.log("Solving for rectangle with l = " + l + " and b = "+ b  );
+
+  if ( l <= 0 || b <= 0){
+    console.log("Rectangle dimentions should be greater than zero : l = " + l + ", and b = "+ b);
+  }
+  else{
+    console.log("The area of the Rectangle is " + rect.area(l,b));
+    console.log("The perimeter of the Rectangle is "+ rect.perimeter(l,b));
+  }
+}
+
+solveRect(2,4);
+solveRect(-12,3);
+solveRect(1,6);
+solveRect(12,14);
