acf604675c30502d1670ec6e946f9ac7245b9382
heniam1 <heniam2012@gmail.com>
2020-02-24 18:35:20 -0500

    Mongoose part 3
    All DeprecationWarning are fixed.

--- node-mongoose/index.js
+++ node-mongoose/index.js
@@ -1,33 +1,54 @@
 const mongoose = require('mongoose');
+mongoose.set('useFindAndModify', false);
 
 const Dishes = require('./models/dishes');
 
 const url = 'mongodb://localhost:27017/conFusion';
-const connect = mongoose.connect(url);
+const connect = mongoose.connect(url , {
+              useCreateIndex: true,
+              useUnifiedTopology: true,
+              useNewUrlParser: true
+            });
 
 connect.then((db) => {
 
     console.log('Connected correctly to server');
 
     Dishes.create({
-          name: 'Uthapizza',
-          description: 'Test'
-      })
-      .then((dish) => {
-          console.log(dish);
+        name: 'Uthappizza',
+        description: 'test'
+    })
+    .then((dish) => {
+        console.log(dish);
 
-          return Dishes.find({}).exec();
-      })
-      .then((dishes) => {
-          console.log(dishes);
+        return Dishes.findByIdAndUpdate(dish._id, {
+            $set: { description: 'Updated test'}
+        },{
+            new: true
+        })
+        .exec();
+    })
+    .then((dish) => {
+        console.log(dish);
 
-          return Dishes.deleteOne({});
-      })
-      .then(() => {
-          return mongoose.connection.close();
-      })
-      .catch((err) => {
-          console.log(err);
-      });
+        dish.comments.push({
+            rating: 5,
+            comment: 'I\'m getting a sinking feeling!',
+            author: 'Leonardo di Carpaccio'
+        });
+
+        return dish.save();
+    })
+    .then((dish) => {
+        console.log(dish);
+
+        return Dishes.deleteOne({});
+    })
+    .then(() => {
+        return mongoose.connection.close();
+    })
+    .catch((err) => {
+        console.log(err);
+    });
 
 });
