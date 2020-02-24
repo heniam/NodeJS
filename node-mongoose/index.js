c7b78baa9d3202f3b67e117d789e43de65445edf
heniam1 <heniam2012@gmail.com>
2020-02-24 18:03:24 -0500

    Mongoose part 2

--- node-mongoose/index.js
+++ node-mongoose/index.js
@@ -9,27 +9,25 @@ connect.then((db) => {
 
     console.log('Connected correctly to server');
 
-    var newDish = Dishes({
-        name: 'Uthappizza',
-        description: 'test'
-    });
+    Dishes.create({
+          name: 'Uthapizza',
+          description: 'Test'
+      })
+      .then((dish) => {
+          console.log(dish);
 
-    newDish.save()
-        .then((dish) => {
-            console.log(dish);
+          return Dishes.find({}).exec();
+      })
+      .then((dishes) => {
+          console.log(dishes);
 
-            return Dishes.find({});
-        })
-        .then((dishes) => {
-            console.log(dishes);
-
-            return Dishes.deleteOne({});
-        })
-        .then(() => {
-            return mongoose.connection.close();
-        })
-        .catch((err) => {
-            console.log(err);
-        });
+          return Dishes.deleteOne({});
+      })
+      .then(() => {
+          return mongoose.connection.close();
+      })
+      .catch((err) => {
+          console.log(err);
+      });
 
 });
