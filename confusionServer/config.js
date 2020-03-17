697d39881f321316912cbe7d9761784d7640ef64
heniam1 <heniam2012@gmail.com>
2020-03-17 03:28:16 -0400

    Passport Facebook
    OAuth autentication facebook-passport

--- confusionServer/config.js
+++ confusionServer/config.js
@@ -1,4 +1,8 @@
 module.exports = {
   'secretKey' : '12345-67890-09876-54321',
-  'mongoUrl' : 'mongodb://localhost:27017/conFusion'
+  'mongoUrl' : 'mongodb://localhost:27017/conFusion',
+  'facebook' : {
+      clientId: '516257965752436',
+      clientSecret: 'e8fff171b4cd28284eb266ff3b323315'
+  }
 }
