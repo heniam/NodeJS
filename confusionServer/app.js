9de30ffc5aa7916bb7520bc1a890a258c36af3e5
heniam1 <heniam2012@gmail.com>
2020-03-18 03:06:28 -0400

    Assignment 4
    Added favorites on server side, (clients can add favorites.)

--- confusionServer/app.js
+++ confusionServer/app.js
@@ -10,6 +10,7 @@ var dishRouter = require('./routes/dishRouter');
 var promoRouter  = require('./routes/promoRouter');
 var leaderRouter = require('./routes/leaderRouter');
 var uploadRouter = require('./routes/uploadRouter');
+var favoriteRouter = require('./routes/favoriteRouter');
 
 var session = require('express-session');
 var FileStore = require('session-file-store')(session);
@@ -69,6 +70,7 @@ app.use('/dishes',dishRouter);
 app.use('promotions',promoRouter);
 app.use('/leaders',leaderRouter);
 app.use('/imageUpload',uploadRouter);
+app.use('/favorites',favoriteRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
